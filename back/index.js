afba0dcb2f499518a2ca60f4da035046dfb5310e
cerramaximiliano <cerramaximiliano@gmail.com>
2023-08-23 15:05:44 -0300

    commit server

---
+++ back/index.js
@@ -0,0 +1,6 @@
+const http = require('http');
+
+http.createServer( function (req,res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'})
+    res.end('Hola Mundo');
+}).listen(3001, 'localhost')
---
+++ front/src/components/Persona.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import style from "./css/Persona.module.css";
+import { connect } from "react-redux";
+import { removeAlumno } from "../redux/actions";
+
+const sDiv = {
+  backgroundColor: "yellow",
+  width: "180px",
+  border: "1px solid grey",
+  borderRadius: "10px",
+  padding: "0 15px 15px 15px",
+  color: "black",
+};
+
+const Alumno = styled.span`
+  color: purple;
+  &:hover {
+    color: violet;
+    cursor: pointer;
+  }
+`;
+
+function Persona({ nombre, apellido, id, created, removeAlumno }) {
+  return (
+    <div style={sDiv}>
+      <h4>
+        Alumno:
+        <Link to={`/detalle/${id}`}>
+          <Alumno>{` ${nombre} ${apellido}`}</Alumno>
+        </Link>
+      </h4>
+
+      {/* <h5 className={style.nombre}>{`${nombre} ${apellido}`}</h5> */}
+      {/* <p id={style.apellido}>{apellido}</p> */}
+      <span style={{ display: "flex", justifyContent: "space-between" }}>
+        <small style={{ color: "green" }}>ID: {id}</small>
+        {created && (
+          <button className={style.btn} onClick={() => removeAlumno(id)}>
+            Borrar
+          </button>
+        )}
+      </span>
+    </div>
+  );
+}
+
+export function mapDispatchToProps(dispatch) {
+  return {
+    removeAlumno: function (id) {
+      dispatch(removeAlumno(id));
+    },
+  };
+}
+
+export default connect(null, mapDispatchToProps)(Persona);
